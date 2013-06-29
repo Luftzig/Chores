e22c77e6f93f9f14ab6a5029e3f722c9aff1c904
shanipolak <spolak@SPOLAK-MOBL.ger.corp.intel.com>
2013-06-30 00:37:27 +0300

    a very very un-finished chore card

---
+++ Chores/src/il/ac/huji/chores/ChoreCardActivity.java
@@ -0,0 +1,14 @@
+package il.ac.huji.chores;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChoreCardActivity extends Activity {
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_chore_card);
+	}
+}
