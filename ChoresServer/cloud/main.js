4b08521c75e3868e8523346b6f48edbcd4d9d066
Yoav Luft <yoav.luft@gmail.com>
2013-08-16 14:01:05 +0300

    Sending push from server!

--- ChoresServer/cloud/main.js
+++ ChoresServer/cloud/main.js
@@ -4,3 +4,13 @@
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+Parse.Cloud.define("test", function(request, response) {
+    Parse.Push.send({
+            where: new Parse.Query(Parse.Installation),
+            data: {
+                alert: "Push test!"
+            }
+    });
+    response.success("Push sent");
+});
