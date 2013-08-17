b528f5d4f213a92b680ae5576e8dbfbce3f6449e
Yoav Luft <yoav.luft@gmail.com>
2013-08-17 16:46:46 +0300

    Added stub invite method to server

--- ChoresServer/cloud/main.js
+++ ChoresServer/cloud/main.js
@@ -14,3 +14,37 @@ Parse.Cloud.define("test", function(request, response) {
     });
     response.success("Push sent");
 });
+
+Parse.Cloud.define("invite", function(request, response) {
+    var user = request.user, name = request.params.name, 
+        phone = request.params.phone, email = request.params.email;
+    var queryPhone = new Parse.Query(Parse.User);
+    if (phone != null && phone !== undefined && phone != "") {
+        queryPhone.equalTo("phone", phone);
+        var query = queryPhone;
+    }
+    var queryEmail = new Parse.Query(Parse.User);
+    if (email != null && email !== undefined && email != "") {
+        queryEmail.equalTo("email", email);
+        if (query) {
+            query = Parse.Query.or(queryPhone, queryEmail);
+        } else {
+            query = queryEmail;
+        }
+    }
+    console.log("invite: querying for user " + name + ", " + phone + ", " + email);
+    query.find({
+            success: function(results) {
+                if (results.length > 0) {
+                    // TODO: find Installation object and send notification
+                    console.log("Alert user " + results[0]);
+                }
+            },
+
+            error: function(error) {
+                // TODO: send user an invite using email or SMS
+                console.log("No such user");
+            }
+    });
+    response.success(name + " was invited.");
+});
