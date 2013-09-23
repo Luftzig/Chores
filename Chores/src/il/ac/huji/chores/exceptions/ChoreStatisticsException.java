396102d5335efd8bf96b9e605c2eb68fb057b1be
atovkun <anna.tovkun@mail.huji.ac.il>
2013-09-23 19:44:20 +0300

    add statistics exception

---
+++ Chores/src/il/ac/huji/chores/exceptions/ChoreStatisticsException.java
@@ -0,0 +1,10 @@
+package il.ac.huji.chores.exceptions;
+
+public class ChoreStatisticsException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public ChoreStatisticsException(String message){
+		super(message);
+	}
+}
