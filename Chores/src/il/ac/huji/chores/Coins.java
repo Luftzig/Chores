e14e0872c5df08b568281988fdc8610a414865ae
atovkun <anna.tovkun@mail.huji.ac.il>
2013-09-25 16:37:25 +0300

    add coins

---
+++ Chores/src/il/ac/huji/chores/Coins.java
@@ -0,0 +1,37 @@
+package il.ac.huji.chores;
+
+public class Coins {
+	
+	private String username;
+	private int dept;
+	private int coinsCollected;
+	private String id;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getDept() {
+		return dept;
+	}
+	public void setDept(int dept) {
+		this.dept = dept;
+	}
+	public int getCoinsCollected() {
+		return coinsCollected;
+	}
+	public void setCoinsCollected(int coinsCollected) {
+		this.coinsCollected = coinsCollected;
+	}
+	
+	
+
+}
